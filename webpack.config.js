263bccc38a8910f9029a9501d22a5e9e4cb186e4
Art <102982@inbox.ru>
2023-12-06 22:25:49 +0500

    Настраивает Babel

--- webpack.config.js
+++ webpack.config.js
@@ -28,5 +28,19 @@ module.exports = {
     }),
 
   ],
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          },
+        },
+      },
+    ]
+  }
 
 };
