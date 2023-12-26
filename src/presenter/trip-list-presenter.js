7a2680481bdaefdd9ab5b4c0f50bc38fcd577596
Art <102982@inbox.ru>
2023-12-26 21:09:57 +0500

    открытие одной только формы редактирования

--- src/presenter/trip-list-presenter.js
+++ src/presenter/trip-list-presenter.js
@@ -1,16 +1,17 @@
 import FilterView from '../view/filter-view.js';
 import SortView from '../view/sort-view.js';
 import ListView from '../view/list-view.js';
-import PointView from '../view/point-view.js';
-import EditPointView from '../view/edit-point-view.js';
-import { render, replace } from '../framework/render.js';
+import { render } from '../framework/render.js';
 import EmptyListView from '../view/empty-list-view.js';
+import PointPresenter from './point-presenter.js';
+import { updateItem } from '../utils.js';
 
 export default class TripListPresenter {
   #tripListComponent = new ListView();
   #listContainer = null;
   #filterContainer = null;
   #eventsModel = null;
+  #pointPresenters = new Map();
 
   constructor(listContainer, filterContainer, eventsModel) {
     this.#listContainer = listContainer;
@@ -18,19 +19,26 @@ export default class TripListPresenter {
     this.#eventsModel = eventsModel;
   }
 
-
   init() {
     this.eventsList = [...this.#eventsModel.events];
     this.offersList = [...this.#eventsModel.offers];
     this.destinationsList = [...this.#eventsModel.destinations];
 
-    render(new FilterView({points: this.eventsList}), this.#filterContainer);
+    this.#renderFilters();
 
     if (this.eventsList.length === 0) {
       render(new EmptyListView(), this.#listContainer);
       return;
     }
 
+    this.#renderPointsBoard();
+  }
+
+  #renderFilters() {
+    render(new FilterView({points: this.eventsList}), this.#filterContainer);
+  }
+
+  #renderPointsBoard() {
     render(new SortView(), this.#listContainer);
     render(this.#tripListComponent, this.#listContainer);
 
@@ -40,46 +48,24 @@ export default class TripListPresenter {
   }
 
   #renderPoint(point) {
-    const escapeKeydownHandler = (evt) => {
-      if (evt.key === 'Escape') {
-        evt.preventDefault();
-        replaceEditFormToPoint();
-        document.removeEventListener('keydown', escapeKeydownHandler);
-      }
-    };
-
-    const pointComponent = new PointView({
-      point,
+    const pointPresenter = new PointPresenter({
+      pointsContainer: this.#tripListComponent.element,
       offers: this.offersList,
       destinations: this.destinationsList,
-      onEditBtnClick: () => {
-        replacePointToEditForm();
-        document.addEventListener('keydown', escapeKeydownHandler);
-      }
+      onDataChange: this.#handlePointChange,
+      onModeChange: this.#handleModeChange
     });
-
-    const pointEditComponent = new EditPointView({
-      point,
-      offers: this.offersList,
-      destinations: this.destinationsList,
-      onFormSubmit: () => {
-        replaceEditFormToPoint();
-        document.removeEventListener('keydown', escapeKeydownHandler);
-      },
-      onFormReset: () => {
-        replaceEditFormToPoint();
-        document.removeEventListener('keydown', escapeKeydownHandler);
-      }
-    });
-
-    function replacePointToEditForm() {
-      replace(pointEditComponent, pointComponent);
-    }
-
-    function replaceEditFormToPoint() {
-      replace(pointComponent, pointEditComponent);
-    }
-
-    render(pointComponent, this.#tripListComponent.element);
+    pointPresenter.init(point);
+    this.#pointPresenters.set(point.id, pointPresenter);
   }
+
+  #handlePointChange = (updatedPoint) => {
+    this.eventsList = updateItem(this.eventsList, updatedPoint);
+    this.#pointPresenters.get(updatedPoint.id).init(updatedPoint);
+  };
+
+  #handleModeChange = () => {
+    this.#pointPresenters.forEach((presenter) => presenter.resetFormView());
+  };
+
 }
